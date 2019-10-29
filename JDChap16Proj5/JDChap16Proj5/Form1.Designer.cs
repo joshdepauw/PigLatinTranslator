cad231185a9be3bfd1234fe4461bd3adb90a97f5
joshdepauw <42982226+joshdepauw@users.noreply.github.com>
2019-10-29 10:44:19 -0500

    Add files via upload

---
+++ JDChap16Proj5/JDChap16Proj5/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace JDChap16Proj5
+{
+    partial class Form1
+    {
+        /// <summary>
+/// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.translateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.displayTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Location = new System.Drawing.Point(42, 93);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(396, 20);
+            this.userTextBox.TabIndex = 0;
+            // 
+            // translateButton
+            // 
+            this.translateButton.Location = new System.Drawing.Point(135, 156);
+            this.translateButton.Name = "translateButton";
+            this.translateButton.Size = new System.Drawing.Size(185, 44);
+            this.translateButton.TabIndex = 1;
+            this.translateButton.Text = "Translate To Pig Latin";
+            this.translateButton.UseVisualStyleBackColor = true;
+            this.translateButton.Click += new System.EventHandler(this.translateButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(401, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter some text below to see what it looks like in Pig Latin! (Only letters and s" +
+    "paces)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Translation:";
+            // 
+            // displayTextBox
+            // 
+            this.displayTextBox.Location = new System.Drawing.Point(32, 247);
+            this.displayTextBox.Multiline = true;
+            this.displayTextBox.Name = "displayTextBox";
+            this.displayTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.displayTextBox.Size = new System.Drawing.Size(398, 64);
+            this.displayTextBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 368);
+            this.Controls.Add(this.displayTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.translateButton);
+            this.Controls.Add(this.userTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.Button translateButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox displayTextBox;
+    }
+}
+
---
+++ JDChap16Proj5/JDChap16Proj5/Form1.cs
@@ -0,0 +1,75 @@
+﻿//Josh DePauw
+//Chapter 16 Project 5
+//Pig Latin Translator
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JDChap16Proj5
+{
+    public partial class Form1 : Form
+    {
+
+        //create variables
+        private string pig = "ay";
+        private string userInput;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void translateButton_Click(object sender, EventArgs e)
+        {
+            GetPigLatin();
+        }
+
+        private void GetPigLatin()
+        {
+            // only allows user to enter letters and spaces
+            if (!(System.Text.RegularExpressions.Regex.IsMatch(userTextBox.Text, "^[a-zA-Z ]*$")))
+            {
+                MessageBox.Show("Only enter letters and spaces please re-enter: ");
+                return;
+            }
+
+            userInput = userTextBox.Text; //assigns variable userInput to the text entered in by the user
+            StringBuilder buffer = new StringBuilder();
+            string[] words = userInput.Split(' ');//puts user input into an array seperated by white spaces
+
+            //translate each word individually
+            foreach (var word in words)
+            {
+                try
+                {
+                    //takes the first letter of each word and adds "ay" to the end
+                    string addAy = word[0].ToString() + pig;
+
+                    //takes whats in index 0 and removes it 
+                    string minusFirst = word.Remove(0, 1);
+
+                    //takes the word without first letter and adds 1st letter and "ay" to the end
+                    string pigLatin = minusFirst + addAy;
+
+                    //add to the buffer with space after
+                    buffer.Append(pigLatin + " ");
+                }
+                //catch extra whitespace
+                catch (IndexOutOfRangeException)
+                {
+                    MessageBox.Show("Please delete the extra whitespaces and try again:");
+                    return;
+                }
+            }
+
+            //display whats in the buffer onto the textbox
+            displayTextBox.Text += buffer.ToString() + System.Environment.NewLine;
+        }
+    }
+}
